aa8bc51d88fdfc6e0892adac0f06ab7d870b204f
Mirrer1 <alsejr1004@gmail.com>
2026-06-16 09:50:24 +0900

    refactor: 한글 폰트 자체 호스팅

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,31 +1,37 @@
-import { Gowun_Dodum, Nanum_Gothic, Nanum_Myeongjo, Noto_Sans_KR } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const notoSansKr = Noto_Sans_KR({
-  weight: ['400', '700'],
+const notoSansKr = localFont({
+  src: [
+    { path: '../app/fonts/NotoSansKR-400.woff2', weight: '400' },
+    { path: '../app/fonts/NotoSansKR-700.woff2', weight: '700' },
+  ],
   variable: '--font-noto-sans-kr',
   display: 'swap',
-  preload: false,
 })
 
-const nanumGothic = Nanum_Gothic({
-  weight: ['400', '700'],
+const nanumGothic = localFont({
+  src: [
+    { path: '../app/fonts/NanumGothic-400.woff2', weight: '400' },
+    { path: '../app/fonts/NanumGothic-700.woff2', weight: '700' },
+  ],
   variable: '--font-nanum-gothic',
   display: 'swap',
-  preload: false,
 })
 
-const nanumMyeongjo = Nanum_Myeongjo({
-  weight: ['400', '700'],
+const nanumMyeongjo = localFont({
+  src: [
+    { path: '../app/fonts/NanumMyeongjo-400.woff2', weight: '400' },
+    { path: '../app/fonts/NanumMyeongjo-700.woff2', weight: '700' },
+  ],
   variable: '--font-nanum-myeongjo',
   display: 'swap',
-  preload: false,
 })
 
-const gowunDodum = Gowun_Dodum({
+const gowunDodum = localFont({
+  src: '../app/fonts/GowunDodum-400.woff2',
   weight: '400',
   variable: '--font-gowun-dodum',
   display: 'swap',
-  preload: false,
 })
 
 export const koreanFontVariables = [

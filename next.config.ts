e298c5f92701e04f244ba3d80b1bd34c88f46a09
Mirrer1 <alsejr1004@gmail.com>
2026-06-23 09:54:33 +0900

    fix: 이미지 업로드 Server Action 크기 상향

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // 업로드 이미지 Server Action 크기 상향
+    serverActions: {
+      bodySizeLimit: '6mb',
+    },
+  },
 }
 
 export default nextConfig
